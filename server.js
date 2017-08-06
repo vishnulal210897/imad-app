9a5556101cb26787fbeafaf5e936283d84f1f4d5
vishnulal210897 <30695245+vishnulal210897@users.noreply.github.com>
2017-08-06 14:52:22 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,12 +89,6 @@ app.get('/:articleName', function (req, res) {
     var articleName = req.params.articleName;
  res.send(createtemplate(articles[articleName]));
 });
-app.get('/article-two', function (req, res) {
-  res.send(createtemplate(articletwo));
-});
-app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
