dbc2a2825f61ee8b4e1abe919ad7358aa496e65d
vishnulal210897 <30695245+vishnulal210897@users.noreply.github.com>
2017-08-06 05:55:38 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.send('article 1');
+  res.send('article one');
 });
 
 app.get('/ui/style.css', function (req, res) {
