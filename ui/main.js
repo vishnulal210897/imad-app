3432397c23001ea9dcc43b9d91ecc3ffb3f40be7
vishnulal210897 <30695245+vishnulal210897@users.noreply.github.com>
2017-08-07 14:28:42 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,9 @@
 console.log('Loaded!');
 var element = doucment.getElementById('main-text');
 element.innerHTML = 'new value';
+
+
 var img = doucment.getElementById('madi');
-img.onclick = function(){
+img.onclick = function (){
 element.style.marginleft = "100px";
 };
