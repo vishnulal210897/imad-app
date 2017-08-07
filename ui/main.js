edf2060c5573c11ff1662ab298f039d3ffbd6e0c
vishnulal210897 <30695245+vishnulal210897@users.noreply.github.com>
2017-08-07 14:46:07 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,12 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'new value';
 var img = document.getElementById('maddy');
+var marginleft = 0;
+function moveright () {
+marginleft = marginleft + 1;
+img.style.marginleft = marginleft + 'px';
+}
 img.onclick = function () {
-img.style.marginleft = "100px";
+var interval = setinterval(moveright,50);
 };
+
