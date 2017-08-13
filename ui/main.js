0e09bb5c08826b7920ad73bdee74294a2c3101f0
vishnulal210897 <30695245+vishnulal210897@users.noreply.github.com>
2017-08-13 05:18:47 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,20 @@
 //counter
 var button = document.getElementById('counter');
 button.onclick = function(){
- //   var reguest = newXMLHTTPRequest();
-   // request.onreadystatechange = function(){
-     //   if(requeststate === XMLHTTPRequest.DONE){
-       //     if(request.status === 200){
-        //        var counter = request.response.Text;
-          //      var span = document.getElementById('count');
-        //        span.innerHTML = counter.toString();
-          //  }
-        //}
-    //};
-    //request.open('GET','http://vjvishnu67.imad.hasura-app.io',true);
-    //request.send(null);
-//};
-var counter = 0;
-counter = counter + 1;
-span.innerHTML = counter.toString();
+    var reguest = newXMLHTTPRequest();
+    request.onreadystatechange = function(){
+        if(requeststate === XMLHTTPRequest.DONE){
+            if(request.status === 200){
+                var counter = request.response.Text;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://vjvishnu67.imad.hasura-app.io',true);
+    request.send(null);
 };
+//var counter = 0;
+//counter = counter + 1;
+//span.innerHTML = counter.toString();
+//};
