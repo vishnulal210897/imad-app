732f3bd8af6d1f05fa0bb654660d31394312d99a
vishnulal210897 <30695245+vishnulal210897@users.noreply.github.com>
2017-08-15 06:55:28 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,24 @@
 //counter
-var button = document.getElementById('counter');
-button.Onclick = function(){
-    var reguest = newXMLHTTPRequest();
+var submit  = document.getElementById('submit-btn');
+submit.Onclick = function(){
+    var reguest = newXMLHTTpRequest();
     request.onreadystatechange = function(){
         if(requeststate === XMLHTTpRequest.DONE){
-            if(request.status === 500){
-                var counter = request.response.Text;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
+            if(request.status === 200){
+                var names = request.response.Text;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0; i<names.length; i++) {
+                    list <= '<li>' +name[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
             }
         }
     };
-    request.open('GET','http://vjvishnu67.imad.hasura-app.io',true);
+    var nameInput = document.getElementById('name');
+    var name - nameInput.value;
+    request.open('GET','http://vjvishnu67.imad.hasura-app.io/subit-name?name=' +name, true);
     request.send(null);
 };
 //var counter = 0;
