4b83b7b2722abfcfe871db1ee57d6a4921fd92e9
vishnulal210897 <30695245+vishnulal210897@users.noreply.github.com>
2017-08-24 14:21:58 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.get('/test-db',function(req,res){
         if(err){
             res.status(500).send(err.toString(result));
         } else {
-            res.send(JSON.stringify(result));
+            res.send(JSON.stringify(result.rows));
         }
     });
 });
@@ -113,8 +113,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/:articleName', function (req, res) {
-    var articleName = req.params.articleName;
- res.send(createtemplate(articles[articleName]));
+    Pool.query('SELECT * FROM article WHERE title = $1, [req.params.articleName]', function(err,result) {
+        if(err){
+            res.status(500).send(err.toString());
+        } else {
+            if(result.rows.length === 0) {
+                res.status(404).send('Article not found');
+            } else {
+                var articledata = result.rows[0];
+                res.send(createtemplate(articledata));
+            }
+        }
+    });
 });
 
 app.get('/ui/style.css', function (req, res) {
