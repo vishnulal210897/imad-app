fa39a1f81ea17a194d895b6623e418beaa7b7b4f
vishnulal210897 <30695245+vishnulal210897@users.noreply.github.com>
2017-08-24 14:27:50 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/articles/:articleName', function (req, res) {
-    Pool.query('SELECT * FROM article WHERE title = $1, [req.params.articleName]', function(err,result) {
+    Pool.query('SELECT * FROM article WHERE title = $1', [req.params.articleName], function(err,result) {
         if(err){
             res.status(500).send(err.toString());
         } else {
