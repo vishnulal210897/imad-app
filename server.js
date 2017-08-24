5f2ebcb3f3006004a2e6f2487eea5ed95bfbd259
vishnulal210897 <30695245+vishnulal210897@users.noreply.github.com>
2017-08-24 14:05:32 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.get('/test-db',function(req,res){
         if(err){
             res.status(500).send(err.toString(result));
         } else {
-            res.send(JSON.stringify(result.row));
+            res.send(JSON.stringify(result));
         }
     });
 });
